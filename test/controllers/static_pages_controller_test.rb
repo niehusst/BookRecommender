9c2155bb93749bcc2dc2410f9119306b8ff92e8f
niehusst <liamniehusstaab@gmail.com>
2020-02-04 17:27:33 -0600

    fixed tests to request at the correct URI

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     test "should get home" do
-        get 'static_pages#home'
+        get '/'
         assert_response :success
     end
 
     test "should get about" do
-        get static_pages_about_url
+        get '/about/'
         assert_response :success
     end
 end
