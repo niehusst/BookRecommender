9168703d44103d797e623614afe4328af18b9d83
niehusst <liamniehusstaab@gmail.com>
2020-02-06 17:51:42 -0600

    connected routes for new pages

---
+++ app/controllers/recommendations_controller.rb
@@ -0,0 +1,6 @@
+class RecommendationsController < ApplicationController
+  # GET /recommendation/book/
+  def book_rec
+    #TODO: uses googlebooks gem stuff to throw stuff into the view
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 #  get 'static_pages/home'
   get 'about', to: 'static_pages#about'
+  get 'recommend', to: 'static_pages#recommend'
+#TODO: make pass info in url??
+  get 'recommend/book', to: 'recommendations#book_rec'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
