be3303af14bf6ec752f92c47efb20126a9e99562
niehusst <liamniehusstaab@gmail.com>
2020-02-06 20:37:14 -0600

    added books model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200131014025) do
+ActiveRecord::Schema.define(version: 20200207021216) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "rating"
+    t.string "img"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
