7003005f48d0db8db997db9a4405062c264f5297
niehusst <liamniehusstaab@gmail.com>
2020-02-11 00:55:59 -0600

    added to_fixture command to pull db data for test

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,6 +1,6 @@
 class RecommendationsController < ApplicationController
   # GET /recommendation/book/
   def book_rec
-    #TODO: uses googlebooks gem stuff to throw stuff into the view
+    #TODO: uses googlebooks gem stuff to throw stuff into the view (actually do this in model?)
   end
 end
