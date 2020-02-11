be0d8fc133d57e9cd5e40013d6eb70568406a389
niehusst <liamniehusstaab@gmail.com>
2020-02-10 22:26:30 -0600

    added devise users and linked nav buttons

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'bootstrap',    '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'googlebooks',  '0.0.9'
+gem 'devise',       '4.7.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
 #  get 'static_pages/home'
   get 'about', to: 'static_pages#about'
   get 'recommend', to: 'static_pages#recommend'
 #TODO: make pass books api request query in url??
   get 'recommend/book', to: 'recommendations#book_rec'
-  resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
