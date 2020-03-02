19dedc9b337870717a72827bacbbf800ca7cd91e
niehusst <liamniehusstaab@gmail.com>
2020-03-02 13:54:22 -0600

    adds book model tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    setup do
+
+    end
+
+    test "create valid book" do
+        book = Book.create({title: "Catch-22", author: "Joseph Heller", rating: 5, img: "https://bookurl.com", genre: "fiction"})
+        assert book.valid?, "Valid book was bad"
+        assert Book.find_by(title: "Catch-22"), "Couldn't find book in db"
+    end
+
+    test "read book" do
+        book = books(:book1)
+        assert_equal book.title, "booko"
+    end
+
+    test "update book" do
+        book = books(:book1)
+        book.title = "Turtles all the way down"
+        book.author = "Hank Green"
+        book.save
+        assert book.valid?, "Edited book wasn't valid"
+        assert Book.find_by(author: "Hank Green")
+    end
+
+    test "destroy book" do
+        book = books(:book1)
+        title = book.title
+        book.destroy
+        assert_nil Book.find_by(title: title)
+    end
 end
