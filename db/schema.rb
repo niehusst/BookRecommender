1791a0d66009e38cc65561a1f54b7e8ccbc2353c
niehusst <liamniehusstaab@gmail.com>
2020-03-06 16:22:01 -0600

    added description col to book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200302224729) do
+ActiveRecord::Schema.define(version: 20200306205748) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200302224729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "description"
     t.index ["user_id"], name: "index_book_id"
   end
 
